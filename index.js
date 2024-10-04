cb1476d6bc4d989ee95ee6f4cdbc069b1ea8f4e1
10santiago12 <surrego1303@gmail.com>
2024-10-04 09:03:07 -0500

    final-changes

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const loginRoute = require('./auth'); // Ruta para el login
-const weatherRoute = require('./weather'); // Asegúrate de que este archivo exista y se llame 'weather.js'
+const weatherRoute = require('./weather'); // Ruta para obtener el clima
+const { authToken } = require('./auth'); // Importa el middleware de autenticación
 
 // Middleware para procesar datos JSON
 app.use(express.json());
@@ -11,12 +12,12 @@ app.use(express.json());
 // Ruta para el login
 app.use('/login', loginRoute);
 
-// Ruta para obtener el clima
-app.use('/weather', weatherRoute); // Asegúrate de que esta ruta esté configurada correctamente
+// Ruta protegida para obtener el clima
+app.use('/weather', authToken, weatherRoute); // Asegúrate de que esta ruta esté configurada correctamente
 
 // Ruta principal
 app.get('/', (req, res) => {
-    res.send('Bienvenido!!');
+    res.send('Bienvenido a la app del clima!!');
 });
 
 // Manejar rutas no definidas
